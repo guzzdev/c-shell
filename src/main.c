fa0a0e0777c94477f23a4323403ebe9850798078
Louis Olive <33105201+guzzdev@users.noreply.github.com>
2025-12-17 00:01:41 +0100

    Incorrect if statement

--- src/main.c
+++ src/main.c
@@ -20,10 +20,18 @@ int main(int argc, char *argv[])
       printf("%s\n", input + 5); // Print everything after "echo "
       continue;
     } else if (strncmp(input, "type", 4) == 0) {
-      if (strcmp(input + 5, "exit") == 0) printf("%s is a shell builtin\n", input + 5);
-      if (strcmp(input + 5, "echo") == 0)  printf("%s is a shell builtin\n", input + 5);
-      if (strcmp(input + 5, "type") == 0)  printf("%s is a shell builtin\n", input + 5);
-      else {
+      if (strcmp(input + 5, "exit") == 0) {
+        printf("%s is a shell builtin\n", input + 5); 
+        continue;
+      }
+      if (strcmp(input + 5, "echo") == 0) {
+        printf("%s is a shell builtin\n", input + 5); 
+        continue;
+      } 
+      if (strcmp(input + 5, "type") == 0) {
+        printf("%s is a shell builtin\n", input + 5); 
+        continue;
+      } else {
         printf("%s: not found\n", input + 5);
       }
       continue;
