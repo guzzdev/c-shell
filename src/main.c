93389c4f813148950f88f27825c7eb1fa0a24b8f
Louis Olive <33105201+guzzdev@users.noreply.github.com>
2025-12-16 23:13:16 +0100

    Added first repl step

--- src/main.c
+++ src/main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+int main(int argc, char *argv[]) { 
+  for (;;) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
+    printf("$ ");
+    char input[100];
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
+    printf("%s: command not found\n", input);
+  }
   
-  char input[100];
-  fgets(input, sizeof(input), stdin);
-  input[strcspn(input, "\n")] = '\0';
-  printf("%s: command not found\n", input);
+
+
+
 
   return 0;
 }
