3fb96e4c45f68e5cc5207f074c972ece9de499d3
Louis Olive <33105201+guzzdev@users.noreply.github.com>
2025-12-16 23:08:46 +0100

    Fix command not found message formatting

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
   char input[100];
   fgets(input, sizeof(input), stdin);
   input[strcspn(input, "\n")] = '\0';
-  printf("$%s: command not found\n", input);
+  printf("%s: command not found\n", input);
 
   return 0;
 }
